aaf447318a8fb57dbeafcdc921ce7f915396b6dc
samius-s <serg.mypost@gmail.com>
2021-05-02 20:09:28 +0300

    ready to code

---
+++ www/static/js/main.cb68ba1c.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonptest-android-app"]=this["webpackJsonptest-android-app"]||[]).push([[0],{11:function(e,n,t){"use strict";t.r(n);t(1);var d=t(3),c=t.n(d),r=(t(8),t(9),t(0));var a=function(){return Object(r.jsx)("div",{className:"App",children:Object(r.jsx)("header",{className:"App-header",children:"Hello world"})})},o=function(){c.a.render(Object(r.jsx)(a,{}),document.getElementById("root"))};window.cordova?document.addEventListener("deviceready",o,!1):o()},8:function(e,n,t){},9:function(e,n,t){}},[[11,1,2]]]);
+//# sourceMappingURL=main.cb68ba1c.chunk.js.map
